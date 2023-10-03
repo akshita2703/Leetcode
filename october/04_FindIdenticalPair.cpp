befeb44def2ac481efddda0d3e60af93a88473bc
akshita2703 <rastogi32akshita@gmail.com>
2023-10-03 19:48:47 +0530

    Day3

---
+++ october/04_FindIdenticalPair.cpp
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/number-of-good-pairs/?envType=daily-question&envId=2023-10-03
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums){
+        vector<int> freq(100);
+        int an =0;
+        for(auto &it:nums){
+            freq[it-1]++;
+        }
+        for(auto a:freq){
+            an+=(a-1)*(a)/2;
+        }
+        return an;
+        
+    }
+};
+
+int main(){
+    Solution obj;
+    vector<int> nums{1,2,3,4,1,3,4,5};
+    cout<< obj.numIdenticalPairs(nums);
+
+    return 0;
+}
