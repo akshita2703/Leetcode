210fc19e3099d3661b3c5f5dcfcb4b2031ef628e
akshita2703 <rastogi32akshita@gmail.com>
2023-12-16 00:16:04 +0530

    December

--- october/26_uglynumber.cpp
+++ october/26_uglynumber.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <stack>
+#include <bits/stdc++.h> // This includes all necessary C++ standard library headers.
 using namespace std;
+
 class Solution {
 public:
     vector<int> primes = {2, 3, 5};
+    
     bool isUgly(int n) {
         if (n < 1) return false;
-        for (int p: primes) while (n % p == 0) n /=p;
+        
+        for (int p : primes) 
+            while (n % p == 0) 
+                n /= p;
+        
         return n == 1;
     }
 };
@@ -14,7 +21,7 @@ public:
 int main() {
     Solution solution;
     int n = 2;
-     solution.isUgly(n);
+    cout<<solution.isUgly(n);
     
     return 0;
 }
